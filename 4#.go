b98f8a8d21d0e8950a5af499a9f06a2f38b7b898
za-hancongcong <hancongcong@zhongan.io>
2020-04-22 01:05:22 +0800

    code

---
+++ 4#.go
@@ -0,0 +1,14 @@
+package  main
+
+import "fmt"
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	return  0.0
+}
+
+func main() {
+	num1 := []int {1,2,3}
+	num2 :=  []int {2,3,4}
+	result := findMedianSortedArrays(num1,num2)
+	fmt.Println(result);
+}
