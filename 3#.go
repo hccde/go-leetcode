535f38700c564c2b073a1349c700bdbb88cee2a7
za-hancongcong <hancongcong@zhongan.io>
2020-04-22 00:57:29 +0800

    ugly

--- 3#.go
+++ 3#.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 // 空字符串的边界情况，没说no-empty 行吧
-func compareStrings(res *[]string, index int, list *[]string) *[]string {
+func compareStrings(res *[]string, index int, list *[]string) (*[]string,int){
 	cur := (*list)[index]
+	maxLength := len(*res)
+	//fmt.Println(*res,cur)
 	for i := 0; i < len(*res); i++ {
 		if (*res)[i] == cur {
 			// 有重复
@@ -19,19 +21,17 @@ func compareStrings(res *[]string, index int, list *[]string) *[]string {
 			}
 
 			temIndex := index + 1
-
 			if temIndex <= len(*list)-1 {
-				temRes := compareStrings(&tem, temIndex, list)
+				temRes,maxLength2 := compareStrings(&tem, temIndex, list)
+				var longer *[]string
 				if len(*temRes) >= len(*res) {
-					return temRes
-				}
-			} else {
-				if len(tem)>= len(*res){
-					return &tem
+					longer = temRes
 				}else{
-					//fmt.Println(*res)
-					return res
+					longer = res
 				}
+				return  longer,max(maxLength,maxLength2)
+			} else {
+				return  &tem,max(maxLength,len(tem));
 			}
 
 		}
@@ -40,28 +40,36 @@ func compareStrings(res *[]string, index int, list *[]string) *[]string {
 	if (index < len(*list)) {
 		tem := append(*res, cur)
 		if (index+1 < len(*list)) {
-			temRes := compareStrings(&tem, index+1, list)
-			return  temRes
+			temRes,maxLength2 := compareStrings(&tem, index+1, list)
+			return  temRes,max(maxLength,maxLength2)
 		} else {
-			return &tem
+			return &tem,max(maxLength,len(tem))
 		}
 	}
-	return res
+	return res,maxLength
 }
+
+func max(a int,b int) int{
+	if a > b{
+		return a
+	}else{
+		return b
+	}
+}
+
 func lengthOfLongestSubstring(s string) int {
 	list := strings.Split(s, "")
-	//size := len(list)
 	if len(list) == 0 {
 		return  0
 	}
 	res := make([]string,1)
 	res[0] = list[0]
 	i := 0
-	result := compareStrings(&res, i, &list)
-	return len(*result)
+	_,result := compareStrings(&res, i, &list)
+	return result;
 }
 
 func main() {
-	num := lengthOfLongestSubstring("abcabcbb")
+	num := lengthOfLongestSubstring("abaaaa")
 	fmt.Println(num)
 }
